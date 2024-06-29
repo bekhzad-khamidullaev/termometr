19d91a96b1cd3c30299182bf2dd57f4ef1e7ef9c
Bekhzad Khamidullaev <bekhzad_94@icloud.com>
2024-06-29 23:23:48 +0500

    jwt token refresh token lifetime set to 5 minutes

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ JWT_AUTH = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
